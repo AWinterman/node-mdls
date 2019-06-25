d47a9175a4e2d60e081349968d620108309a2464
Abraham Makovetsky <avimak@gmail.com>
2019-06-25 09:28:34 +0300

    giving date type a chance before looking for chars in str

--- index.js
+++ index.js
@@ -69,12 +69,6 @@ function to_js_type(key) {
       return value.slice(1, -1)
     }
 
-    // handle cases like `("H.264",AAC)` where AAC doesn't
-    // get wrapped with double-quotes
-    if (/[a-zA-Z]+/.test(value)) {
-      return value;
-    }
-
     const as_num = +value
 
     if(!isNaN(as_num)) {
@@ -83,11 +77,17 @@ function to_js_type(key) {
 
     const as_date = new Date(value)
 
-    if(isNaN(as_date.getTime())) {
-      bad_value(key, value)
+    if(!isNaN(as_date.getTime())) {
+      return as_date
     }
 
-    return as_date
+    // handle cases like `("H.264",AAC)` where AAC doesn't
+    // get wrapped with double-quotes
+    if (/[a-zA-Z]+/.test(value)) {
+      return value
+    }
+
+    bad_value(key, value)
   }
 }
 
